d315fcb602fe438fa90f19f5df47452e68036e3b
me <yganzha.unit@gmail.com>
2023-07-10 19:47:39 +0530

    step 1, adding files

---
+++ conftest.py
@@ -0,0 +1,42 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
+
+# для корректного отображения кириллицы в параметризаторах
+def pytest_make_parametrize_id(config, val):
+    return repr(val)
+
+
+# добавляем параметр запуска тестов в командной строке(чем запускать, хромом или фаерфоксом) По умолчанию хром
+def pytest_addoption(parser):
+    # parser.addoption('--browser_name', action='store', default=None, help="Choose browser: chrome or firefox")
+    # Можно задать значение параметра по умолчанию,
+    # чтобы в командной строке не обязательно было указывать параметр --browser_name, например, так:
+    parser.addoption('--browser_name', action='store', default="chrome", help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en", help="Choose language^ for example: en, fr, ko ... etc")
+
+
+# Запуск браузера(для каждой функции)
+@pytest.fixture(scope="function")  # по умолчанию запускается для каждой функции
+def browser(request):
+    language = request.config.getoption("language")
+    browser_name = request.config.getoption("browser_name")  # получаем параметр командной строки browser_name
+    browser = None
+
+    if browser_name == "chrome":
+        print("\nstart Сhrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nstart Firefox browser for test..")
+        options_firefox = OptionsFirefox()
+        options_firefox.set_preference("intl.accept_languages", language)
+        browser = webdriver.Firefox(options=options_firefox)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+
+    print("\nquit browser..")
+    browser.quit()
