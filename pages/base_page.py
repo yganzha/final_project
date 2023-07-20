76a03d78d914659ff373808b49c5bac0f29ad98c
me <yganzha.unit@gmail.com>
2023-07-21 01:04:03 +0530

    delete no needed file

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,4 @@
 class BasePage():
-
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
--- test_main_page.py
+++ test_main_page.py
@@ -2,17 +2,16 @@ from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
 from pages.main_page import MainPage
 
-# link = "http://selenium1py.pythonanywhere.com/"
-
-
-class MainPage(BasePage):
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
-
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
