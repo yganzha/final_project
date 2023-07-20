fd8415e7bfbd393f7bc23ad7c47029240b98a74a
me <yganzha.unit@gmail.com>
2023-07-21 00:52:00 +0530

    тест почти полностью написан в модном стиле Page Object!

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,18 @@
 from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
+from pages.main_page import MainPage
 
-link = "http://selenium1py.pythonanywhere.com/"
+# link = "http://selenium1py.pythonanywhere.com/"
 
 
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
 
 
 def test_guest_can_go_to_login_page(browser):
-   browser.get(link)
-   go_to_login_page(browser)
-
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
