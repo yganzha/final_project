b25ff315bf6214f69f0c27937fbaebd8150fe350
me <yganzha.unit@gmail.com>
2023-08-05 00:33:17 +0530

    Put locators in the locators.py file

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,7 @@
 from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from pages.locators import MainPageLocators
 
 class MainPage(BasePage):
     def go_to_login_page(self):
@@ -8,13 +9,7 @@ class MainPage(BasePage):
         login_link.click()
 
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
-
-
-    def __init__(self, browser, url, timeout=10):
-        self.browser = browser
-        self.url = url
-        self.browser.implicitly_wait(timeout)
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
 
 
     def is_element_present(self, how, what):
